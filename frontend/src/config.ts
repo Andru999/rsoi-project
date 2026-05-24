f558c3afbb551c7927eaa98075dbf58ef96f2754
Andrey Gorynkin <temp@temp.com>
2026-05-24 18:55:35 +0300

    stable v0.1

---
+++ frontend/src/config.ts
@@ -0,0 +1,28 @@
+export const config = {
+  server: {
+    auth: {
+      host: "localhost",
+      port: 8888,
+    },
+    statistics: {
+      host: "localhost",
+      port: 8090,
+    },
+    gateway: {
+      host: "localhost",
+      port: 8080,
+    },
+    ticket: {
+      host: "localhost",
+      port: 8070,
+    },
+    flight: {
+      host: "localhost",
+      port: 8060,
+    },
+    bonus: {
+      host: "localhost",
+      port: 8050,
+    },
+  },
+};
