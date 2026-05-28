053707487569b70bb1a9dbddbcf9e20b035bcaf1
Andrey Gorynkin <temp@temp.com>
2026-05-28 20:38:20 +0300

    add timeout for jwks

--- backend/gateway_service/app/utils/api_requests.py
+++ backend/gateway_service/app/utils/api_requests.py
@@ -1,42 +1,24 @@
 import inspect
-
 import requests
+from fastapi import status
+from requests import Response
 from exceptions.http_exceptions import (
     InvalidRequestException,
     ServiceUnavailableException,
 )
-from fastapi import status
-from requests import Response
 
-
-def __check_status_code(
-    status_code: int,
-    service_name: str,
-) -> None:
+def __check_status_code(status_code: int, service_name: str) -> None:
     method = inspect.stack()[1][3]
     method = " ".join(method.split("_")).title()
-
     if status_code == status.HTTP_503_SERVICE_UNAVAILABLE:
-        raise ServiceUnavailableException(
-            message=f"{service_name} unavailable",
-        )
-    if status_code >= 400:  # noqa: PLR2004
-        raise InvalidRequestException(
-            prefix=method,
-            status_code=status_code,
-        )
+        raise ServiceUnavailableException(message=f"{service_name} unavailable")
+    if status_code >= 400:
+        raise InvalidRequestException(prefix=method, status_code=status_code)
 
-
-def get_request(
-    url: str,
-    params: str | None = None,
-) -> Response:
+def get_request(url: str, params: str | None = None, timeout: float = 5.0) -> Response:
     try:
-        response: Response = requests.get(
-            url=url,
-            params=params,
-        )
-    except:
+        response: Response = requests.get(url=url, params=params, timeout=timeout)
+    except Exception:
         response = Response()
         response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
 
